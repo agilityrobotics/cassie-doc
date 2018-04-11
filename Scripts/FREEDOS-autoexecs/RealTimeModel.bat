53baaf0c3ae6e878aeb32088e2a55f3848918f2e
mikhailjonesagility <35974935+mikhailjonesagility@users.noreply.github.com>
2018-04-11 10:19:43 -0700

    Update RealTimeModel.bat
    Updated script to also delete old real-time application models.

--- Scripts/FREEDOS-autoexecs/RealTimeModel.bat
+++ Scripts/FREEDOS-autoexecs/RealTimeModel.bat
@@ -1,5 +1,6 @@
 doslfn
+FOR %f in (D:\*.DAT) DO DEL %f
+FOR %f in (D:\*.mldatx) DO DEL %f
 copy C:\*.mldatx D: /Y /V
 copy C:\calib.bin D: /Y /V
-FOR %f in (D:\*.DAT) DO DEL %f
 xpcboot slrtkrnl.rtb

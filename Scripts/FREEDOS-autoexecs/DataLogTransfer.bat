54e08aa9179051f3a015503b0d562b201452bbc2
Daniel Bennett <daniel.bennett@agilityrobotics.com>
2018-04-11 16:45:30 -0700

    Updated DataLogTransfer to save files in LOGS folder

--- Scripts/FREEDOS-autoexecs/DataLogTransfer.bat
+++ Scripts/FREEDOS-autoexecs/DataLogTransfer.bat
@@ -1,6 +1,12 @@
 doslfn
-FOR %f in (D:\*.DAT) DO COPY %f C: /Y /V
+IF NOT EXIST C:\LOGS MKDIR C:\LOGS
+
+FOR %f in (D:\*.DAT) DO COPY %f C:\LOGS /Y /V
 FOR %f in (D:\*.DAT) DO DEL %f 
+FOR %f in (D:\*.txt) DO COPY %f C:\LOGS /Y /V
+FOR %f in (D:\*.txt) DO DEL %f
+FOR %f in (D:\*.bin) DO COPY %f C:\LOGS /Y /V
+
 :loop
 beep
 goto loop
